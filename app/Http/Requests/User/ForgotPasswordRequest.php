751e13dd149840b676e9cdad968987e51ed82839
sengokudaikon <cryogen.sxu@gmail.com>
2021-03-25 20:42:11 +0300

    добавлены реквесты для аутентификации пользователя, смены пароля

--- app/Http/Requests/User/ForgotPasswordRequest.php
+++ app/Http/Requests/User/ForgotPasswordRequest.php
@@ -2,7 +2,49 @@
 
 namespace App\Http\Requests\User;
 
-class ForgotPasswordRequest
-{
+use Elegant\Sanitizer\Laravel\SanitizesInput;
+use Illuminate\Foundation\Http\FormRequest;
 
+/**
+ * Класс валидации запроса восстановления пароля аккаунта пользователя.
+ *
+ * @OA\Schema(
+ *     schema="ForgotPasswordRequest",
+ *     title="Запрос на восстановление пароля.",
+ *     type="object",
+ *     required={"email"},
+ *     @OA\Property(
+ *          property="email",
+ *          description="Email пользователя.",
+ *          type="string",
+ *          example="admin@terra.ru"
+ *     )
+ * )
+ * @package App\Http\Requests\User
+ */
+class ForgotPasswordRequest extends FormRequest
+{
+    use SanitizesInput;
+
+    /**
+     * Правила валидации.
+     *
+     * @return array|string[]
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'bail|required|email'
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function filters()
+    {
+        return [
+            'email' => 'trim'
+        ];
+    }
 }
--- app/Http/Requests/User/ResetPasswordRequest.php
+++ app/Http/Requests/User/ResetPasswordRequest.php
@@ -2,7 +2,42 @@
 
 namespace App\Http\Requests\User;
 
-class ResetPasswordRequest
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * Класс валидации запроса для установки нового пароля пользователя.
+ *
+ * @OA\Schema(
+ *     title="Запрос на редактирование пароля.",
+ *     type="object",
+ *     required={"token", "password"},
+ *     @OA\Property(
+ *          property="token",
+ *          type="string",
+ *          title="Токен сброса пароля."
+ *     ),
+ *     @OA\Property(
+ *          property="password",
+ *          type="string",
+ *          title="Новый пароль.",
+ *          minLength=5
+ *     )
+ * )
+ * @package App\Http\Requests\User
+ */
+class ResetPasswordRequest extends FormRequest
 {
+    /**
+     * Правила валидации.
+     *
+     * @return string[]
+     */
+    public function rules()
+    {
+        return [
+            'token' => 'required|string',
+            'password' => 'required|min:5'
+        ];
+    }
 
 }
