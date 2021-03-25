b95435ed752c24cfad37e15ef77a2e5c9576843e
sengokudaikon <cryogen.sxu@gmail.com>
2021-03-25 20:42:11 +0300

    добавлены реквесты для аутентификации пользователя, смены пароля

--- app/Http/Requests/User/AuthenticateUserRequest.php
+++ app/Http/Requests/User/AuthenticateUserRequest.php
@@ -2,7 +2,62 @@
 
 namespace App\Http\Requests\User;
 
-class AuthenticateUserRequest
-{
+use Elegant\Sanitizer\Laravel\SanitizesInput;
+use Illuminate\Foundation\Http\FormRequest;
 
+/**
+ * Класс валидации запроса для аутентификации пользователя.
+ *
+ * @OA\Schema(
+ *     title="Запрос аутентификации пользователя.",
+ *     type="object",
+ *     required={"email", "password"},
+ *     @OA\Property(
+ *          property="email",
+ *          description="Email.",
+ *          type="string",
+ *          example="admin@terra.ru"
+ *     ),
+ *     @OA\Property(
+ *          property="password",
+ *          description="Пароль пользователя.",
+ *          type="string",
+ *          example="123456",
+ *          minLength=6
+ *     ),
+ *     @OA\Property(
+ *          property="rememberMe",
+ *          description="Флаг сохренения состояния сессии.",
+ *          type="boolean",
+ *          example="true"
+ *     )
+ * )
+ * @package App\Http\Requests\User
+ */
+class AuthenticateUserRequest extends FormRequest
+{
+    use SanitizesInput;
+
+    /**
+     * Проверка валидации.
+     *
+     * @return string[]
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'bail|required|string|email',
+            'password' => 'bail|required|string|min:6'
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function filters()
+    {
+        return [
+            'email' => 'trim'
+        ];
+    }
 }
