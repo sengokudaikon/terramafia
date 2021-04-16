ea6b74023198750dbe96756d8028325368f6c6cd
sengokudaikon <cryogen.sxu@gmail.com>
2021-04-16 14:55:34 +0300

    del

--- config/skillservice.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'api_url' => env('SKILLSERVICE_URL', 'https://skills.geecko.ru/'),
-    'api_key' => env('SKILLSERVICE_KEY'),
-    'x_company_id' => env('SKILLSERVICE_X_COMPANY_ID'),
-    'prefix' => env('SKILLSERVICE_ROUTE_PREFIX', ''),
-];
